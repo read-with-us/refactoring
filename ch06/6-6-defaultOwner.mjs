f19f24ede6df132b98e334db5c4c0ffed71e4afb
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 00:57:43 +0900

    [examples] 6.6 변수 캡슐화하기 - defaultOwner (4)

--- ch06/6-6-defaultOwner.mjs
+++ ch06/6-6-defaultOwner.mjs
@@ -4,11 +4,11 @@
 
 let defaultOwner = { firstName: '마틴', lastName: '파울러' };
 
-function getDefaultOwner() {
+export function getDefaultOwner() {
   return defaultOwner;
 }
 
-function setDefaultOwner(arg) {
+export function setDefaultOwner(arg) {
   defaultOwner = arg;
 }
 
