09a7f2e874f3976a4af4cef24ddb70c054e8e0a5
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-10 23:21:06 +0900

    [examples] 6.2 함수 인라인하기 - rating (1)

--- ch06/6-2-rating.js
+++ ch06/6-2-rating.js
@@ -3,9 +3,5 @@
  */
 
 function rating(aDriver) {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-
-  function moreThanFiveLateDeliveries(dvr) {
-    return dvr.numberOfLateDeliveries > 5;
-  }
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
