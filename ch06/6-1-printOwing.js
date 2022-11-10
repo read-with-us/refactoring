e8c9642590363295c59c53a5795a7ad2fd3cacda
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-10 22:20:30 +0900

    [examples] 6.1 함수 추출하기 - printOwing 예시

---
+++ ch06/6-1-printOwing.js
@@ -0,0 +1,48 @@
+/**
+ * p161 예시
+ */
+
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  console.log('*******************');
+  console.log('**** 고객 채무 ****');
+  console.log('*******************');
+
+  // 미해결 채무(outstanding) 계산
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  // 마감일 기록
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  // 세부사항 출력
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const Clock = {
+  today: {
+    getFullYear: () => 2022,
+    getMonth: () => 11,
+    getDate: () => 10,
+  },
+};
+
+const invoice = {
+  customer: '마틴 파울러',
+  orders: [{ amount: 1000 }, { amount: 200 }],
+};
+
+printOwing(invoice);
