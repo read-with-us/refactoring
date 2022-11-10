8993039eff470ed63e9e2e54df069dc4b022b477
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-10 23:40:35 +0900

    [examples] 6.2 함수 인라인하기 - reportLines (1)

--- ch06/6-2-reportLines.js
+++ ch06/6-2-reportLines.js
@@ -4,11 +4,11 @@
 
 function reportLines(aCustomer) {
   const lines = [];
+  lines.push(['name', aCustomer.name]);
   gatherCustomerData(lines, aCustomer);
   return lines;
 
   function gatherCustomerData(out, aCustomer) {
-    out.push(['name', aCustomer.name]);
     out.push(['location', aCustomer.location]);
   }
 }
