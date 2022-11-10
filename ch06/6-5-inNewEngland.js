fc4f88432c1f2cc46140ad63e1b6bcf8ca73d879
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 00:41:38 +0900

    [examples] 6.5 함수 선언 바꾸기 - inNewEngland (2)

--- ch06/6-5-inNewEngland.js
+++ ch06/6-5-inNewEngland.js
@@ -4,6 +4,10 @@
 
 function inNewEngland(aCustomer) {
   const stateCode = aCustomer.address.state;
+  return xxNEWinNewEngland(stateCode);
+}
+
+function xxNEWinNewEngland(stateCode) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
 }
 
