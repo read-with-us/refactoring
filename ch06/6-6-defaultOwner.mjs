10f7b9e953451651ac3c1bd20853457249787b7f
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 00:51:30 +0900

    [examples] 6.6 변수 캡슐화하기 - defaultOwner 예시

---
+++ ch06/6-6-defaultOwner.mjs
@@ -0,0 +1,24 @@
+/**
+ * p190 예시
+ */
+
+let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const spaceship = {
+  owner: '',
+};
+
+/**
+ * 예시 참조 & 갱신
+ */
+
+spaceship.owner = defaultOwner;
+
+defaultOwner = { firstName: '레베카', lastName: '파슨스' };
+
+console.log('spaceship owner: ', spaceship.owner);
+console.log('default owner: ', defaultOwner);
