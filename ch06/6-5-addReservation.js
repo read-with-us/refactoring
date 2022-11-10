ecbac13546955ef66438aa1a8af7af145c20a05f
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 00:17:28 +0900

    [examples] 6.5 함수 선언 바꾸기 - addReservation 예시

---
+++ ch06/6-5-addReservation.js
@@ -0,0 +1,27 @@
+/**
+ * p184 예시
+ */
+
+class Book {
+  constructor() {
+    this._reservations = [];
+  }
+
+  get reservations() {
+    return this._reservations;
+  }
+
+  addReservation(customer) {
+    this._reservations.push(customer);
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const book = new Book();
+const customer = { name: '마틴 파울러' };
+book.addReservation(customer);
+
+console.log(book.reservations);
