e4a82d24a3fc138ff2609c283427ba38a495eaf8
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-10 23:50:51 +0900

    [examples] 6.3 변수 추출하기 - price (5)

--- ch06/6-3-price.js
+++ ch06/6-3-price.js
@@ -3,11 +3,11 @@
  */
 
 function price(order) {
-  // 가격(price) = 기본 가격 - 수량 할인 + 배송비
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount =
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
 }
 
 /**
