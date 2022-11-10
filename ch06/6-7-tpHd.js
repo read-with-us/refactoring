3f137d05925403ed24671c748bb501f762a88746
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 01:10:00 +0900

    [examples] 6.7 변수 이름 바꾸기 - tpHd 예시

---
+++ ch06/6-7-tpHd.js
@@ -0,0 +1,25 @@
+/**
+ * p195 예시
+ */
+
+let tpHd = 'untitled';
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+let result = '';
+const obj = {
+  articleTitle: '제목',
+};
+
+/**
+ * 예시 참조 & 수정
+ */
+
+result += `<h1>${tpHd}</h1>`;
+
+tpHd = obj['articleTitle'];
+
+console.log(result);
+console.log(tpHd);
