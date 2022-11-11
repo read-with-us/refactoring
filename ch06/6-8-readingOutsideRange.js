5012606df378f603aa6afa7f5d587f12d4f397f9
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 21:44:34 +0900

    [examples] 6.8 매개변수 객체 만들기 - readingOutsideRange 예시

---
+++ ch06/6-8-readingOutsideRange.js
@@ -0,0 +1,39 @@
+/**
+ * p198 예시
+ */
+
+const station = {
+  name: 'ZB1',
+  readings: [
+    { temp: 47, time: '2016-11-10 09:10' },
+    { temp: 53, time: '2016-11-10 09:20' },
+    { temp: 58, time: '2016-11-10 09:30' },
+    { temp: 53, time: '2016-11-10 09:40' },
+    { temp: 51, time: '2016-11-10 09:50' },
+  ],
+};
+
+function readingsOutsideRange(station, min, max) {
+  return station.readings.filter((r) => r.temp < min || r.temp > max);
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const operatingPlan = {
+  temperatureFloor: 40,
+  temperatureCeil: 52,
+};
+
+/**
+ * 예시 호출
+ */
+
+alerts = readingsOutsideRange(
+  station,
+  operatingPlan.temperatureFloor,
+  operatingPlan.temperatureCeil
+);
+
+console.log(alerts);
