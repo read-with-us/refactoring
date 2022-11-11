a209e6c0060e00fd7164b690e6c20d2ec8c2e2c4
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-12 07:44:55 +0900

    [examples] 6.11 단계 쪼개기 - main (5)

--- ch06/6-11-main.java
+++ ch06/6-11-main.java
@@ -22,7 +22,8 @@ private static long countOrders(CommandLine commandLine, String[] args, String f
   File input = Paths.get(filename).toFile();
   ObjectMapper mapper = new ObjectMapper();
   Order[] orders = mapper.readValue(input, Order[].class);
-  if(Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+  boolean onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
+  if(onlyCountReady)
     return Stream.of(orders).filter(o -> "ready".equals(o.status)).count();
   else
     return orders.length;
