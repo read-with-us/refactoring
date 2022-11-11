a429621b29e13d9bf5e0ad173b048aa1bee31348
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 23:57:49 +0900

    [examples] 6.11 단계 쪼개기 - priceOrder 예시

---
+++ ch06/6-11-priceOrder.js
@@ -0,0 +1,40 @@
+/**
+ * p216 예시
+ */
+
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const product = {
+  basePrice: 10000,
+  discountThreshold: 5,
+  discountRate: 0.1,
+};
+
+const quantity = 10;
+
+const shippingMethod = {
+  feePerCase: 3000,
+  discountThreshold: 50000,
+  discountedFee: 0,
+};
+
+const price = priceOrder(product, quantity, shippingMethod);
+
+console.log(price);
