60cb0a92d1f15a157a6da6e92f63c8ce787c6171
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 22:24:40 +0900

    [examples] 6.9 여러 함수를 클래스로 묶기 - reading (1)

--- ch06/6-9-reading.js
+++ ch06/6-9-reading.js
@@ -4,6 +4,28 @@
 
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+}
+
 /**
  * 예시 실행을 위한 임의의 코드
  */
@@ -50,7 +72,7 @@ console.log(taxableCharge);
  */
 
 const aReading3 = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading3); // <- 기본 요금 계산 함수
+const basicChargeAmount = calculateBaseCharge(aReading3);
 
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
