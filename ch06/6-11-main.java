4d81ad84894a40ae693d5572c05dbae4beb1d642
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-12 07:23:56 +0900

    [examples] 6.11 단계 쪼개기 - main 예시

---
+++ ch06/6-11-main.java
@@ -0,0 +1,22 @@
+/**
+ * p220 예시 (동작하지 않는 코드)
+ */
+
+public static void main(String[] args) {
+  try {
+    if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+    String filename = args[args.length - 1];
+    File input = Paths.get(filename).toFile();
+    ObjectMapper mapper = new ObjectMapper();
+    Order[] orders = mapper.readValue(input, Order[].class);
+    if(Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+      System.out.println(Stream.of(orders)
+                               .filter(o -> "ready".equals(o.status))
+                               .count());
+    else
+      System.out.println(orders.length);
+  } catch (Exception e) {
+    System.err.println(e);
+    System.exit(1);
+  }
+}
