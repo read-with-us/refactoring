ecc9f2aa053711bba7a842f6b263468e2738837b
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 21:47:50 +0900

    [examples] 6.8 매개변수 객체 만들기 - readingOutsideRange (1)

--- ch06/6-8-readingOutsideRange.js
+++ ch06/6-8-readingOutsideRange.js
@@ -13,6 +13,19 @@ const station = {
   ],
 };
 
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min, max };
+  }
+
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+}
+
 function readingsOutsideRange(station, min, max) {
   return station.readings.filter((r) => r.temp < min || r.temp > max);
 }
