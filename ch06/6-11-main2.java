228de924d5448950b90994f559737b1a6bcf72b5
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-12 07:56:17 +0900

    [examples] 6.11 단계 쪼개기 - main2 예시

---
+++ ch06/6-11-main2.java
@@ -0,0 +1,31 @@
+/**
+ * p228 예시 (동작하지 않는 코드)
+ */
+
+public static void main(String[] args) {
+  try {
+    System.out.println(run(args));
+  } catch (Exception e) {
+    System.err.println(e);
+    System.exit(1);
+  }
+}
+
+static void run(String[] args) throws IOException {
+  if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+  CommandLine commandLine = new CommandLine();
+  String filename = args[args.length - 1];
+  return countOrders(commandLine, args, filename);
+}
+
+private static long countOrders(CommandLine commandLine, String[] args, String filename) throws IOException {
+  File input = Paths.get(filename).toFile();
+  ObjectMapper mapper = new ObjectMapper();
+  Order[] orders = mapper.readValue(input, Order[].class);
+  if(Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+    return Stream.of(orders).filter(o -> "ready".equals(o.status)).count();
+  else
+    return orders.length;
+}
+
+private static class CommandLine {}
