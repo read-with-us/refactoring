bb923ddafdec1f84820968ef1a7856d34bdb13e2
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-12 07:52:14 +0900

    [examples] 6.11 단계 쪼개기 - main (9)

--- ch06/6-11-main.java
+++ ch06/6-11-main.java
@@ -12,11 +12,16 @@ public static void main(String[] args) {
 }
 
 static void run(String[] args) throws IOException {
+  CommandLine commandLine = parseCommandLine(args);
+  return countOrders(commandLine);
+}
+
+private static CommandLine parseCommandLine(String[] args) {
   if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
   CommandLine commandLine = new CommandLine();
   commandLine.filename = args[args.length - 1];
   commandLine.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
-  return countOrders(commandLine);
+  return commandLine;
 }
 
 private static long countOrders(CommandLine commandLine) throws IOException {
