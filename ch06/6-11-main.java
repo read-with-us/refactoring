1ebc3f0e06fdabd6b6117b544a3e829be9b923aa
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-12 07:30:53 +0900

    [examples] 6.11 단계 쪼개기 - main (1)

--- ch06/6-11-main.java
+++ ch06/6-11-main.java
@@ -4,19 +4,23 @@
 
 public static void main(String[] args) {
   try {
-    if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-    String filename = args[args.length - 1];
-    File input = Paths.get(filename).toFile();
-    ObjectMapper mapper = new ObjectMapper();
-    Order[] orders = mapper.readValue(input, Order[].class);
-    if(Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
-      System.out.println(Stream.of(orders)
-                               .filter(o -> "ready".equals(o.status))
-                               .count());
-    else
-      System.out.println(orders.length);
+    run(args);
   } catch (Exception e) {
     System.err.println(e);
     System.exit(1);
   }
+}
+
+static void run(String[] args) throws IOException {
+  if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+  String filename = args[args.length - 1];
+  File input = Paths.get(filename).toFile();
+  ObjectMapper mapper = new ObjectMapper();
+  Order[] orders = mapper.readValue(input, Order[].class);
+  if(Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+    System.out.println(Stream.of(orders)
+                              .filter(o -> "ready".equals(o.status))
+                              .count());
+  else
+    System.out.println(orders.length);
 }
