2a2626e8f0ca7503bdee9ca42589da56b4be1ef9
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-12 00:13:59 +0900

    [examples] 6.11 단계 쪼개기 - priceOrder (6)

--- ch06/6-11-priceOrder.js
+++ ch06/6-11-priceOrder.js
@@ -3,17 +3,23 @@
  */
 
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
+  // <- 첫 번째 단계를 처리하는 함수
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = { basePrice, quantity, discount };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return { basePrice, quantity, discount };
 }
 
 function applyShipping(priceData, shippingMethod) {
+  // <- 두 번째 단계를 처리하는 함수
   const shippingPerCase =
     priceData.basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
