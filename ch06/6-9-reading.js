462cb8c48c56dbb443b313438ff6afa19ce08158
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-11 22:21:00 +0900

    [examples] 6.9 여러 함수를 클래스로 묶기 - reading 예시

---
+++ ch06/6-9-reading.js
@@ -0,0 +1,59 @@
+/**
+ * p204 예시
+ */
+
+const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+function acquireReading() {
+  return reading;
+}
+
+function baseRate(month, year) {
+  if (year >= 2017 && month >= 5) {
+    return 0.3;
+  }
+  return 0.2;
+}
+
+function taxThreshold(year) {
+  if (year < 2015) {
+    return 0.2;
+  }
+  return 0.1;
+}
+
+/**
+ * 예시 호출 (1)
+ */
+
+const aReading = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+console.log(baseCharge);
+
+/**
+ * 예시 호출 (2)
+ */
+
+const aReading2 = acquireReading();
+const base = baseRate(aReading2.month, aReading2.year) * aReading.quantity;
+const taxableCharge = Math.max(0, base - taxThreshold(aReading2.year));
+
+console.log(taxableCharge);
+
+/**
+ * 예시 호출 (3)
+ */
+
+const aReading3 = acquireReading();
+const basicChargeAmount = calculateBaseCharge(aReading3); // <- 기본 요금 계산 함수
+
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+console.log(basicChargeAmount);
