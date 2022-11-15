095929636145ff5d206de6cb82e0f1c22ed8638a
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-15 20:44:22 +0900

    ♻️  7.1 레코드 캡슐화하기 - organization (1)

--- ch07/7-1-organization.js
+++ ch07/7-1-organization.js
@@ -4,6 +4,10 @@
 
 const organization = { name: '애크미 구스베리', country: 'GB' };
 
+function getRawDataOfOrganization() {
+  return organization;
+}
+
 /**
  * 예시 실행을 위한 임의의 코드
  */
@@ -14,8 +18,11 @@ let result = '';
  * 예시 코드 사용
  */
 
-result += `<h1>${organization.name}</h1>`; // <- 읽기 예
-organization.name = 'newName'; // <- 쓰기 예
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+getRawDataOfOrganization().name = 'newName';
 
 console.log(result);
-console.log(organization.name, organization.country);
+console.log(
+  getRawDataOfOrganization().name,
+  getRawDataOfOrganization().country
+);
