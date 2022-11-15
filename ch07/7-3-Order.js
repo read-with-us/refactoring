9bbf32d88bb4d028a4b75a82cf685ac4076a2ae3
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-15 23:45:28 +0900

    ♻️  7.3 기본형을 객체로 바꾸기 - Order (1)

--- ch07/7-3-Order.js
+++ ch07/7-3-Order.js
@@ -3,8 +3,16 @@
  */
 
 class Order {
+  #priority;
   constructor(data) {
-    this.priority = data.priority;
+    this.#priority = data.priority;
+  }
+
+  get priority() {
+    return this.#priority;
+  }
+  set priority(aString) {
+    this.#priority = aString;
   }
 }
 
