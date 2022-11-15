7641f4eef89ce3f3b48dcce940980c92f65d1695
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-15 23:19:50 +0900

    ✨ 7.2 컬렉션 캡슐화하기 - Person

---
+++ ch07/7-2-Person.js
@@ -0,0 +1,68 @@
+/**
+ * p248 예시
+ */
+
+class Person {
+  #name;
+  #courses;
+  constructor(name) {
+    this.#name = name;
+    this.#courses = [];
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get courses() {
+    return this.#courses;
+  }
+  set courses(aList) {
+    this.#courses = aList;
+  }
+}
+
+class Course {
+  #name;
+  #isAdvanced;
+  constructor(name, isAdvanced) {
+    this.#name = name;
+    this.#isAdvanced = isAdvanced;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get isAdvanced() {
+    return this.#isAdvanced;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const filename = 'refactoring';
+
+const courses = {
+  refactoring: ['Encapsulate Record', 'Encapsulate Collection'],
+};
+
+function readBasicCourseNames(filename) {
+  return courses[filename];
+}
+
+/**
+ * 예시 코드 사용
+ */
+
+const aPerson = new Person('kim');
+const numAdvancedCourses = aPerson.courses.filter((c) => c.isAdvanced).length;
+for (const name of readBasicCourseNames(filename)) {
+  aPerson.courses.push(new Course(name, false));
+}
+
+console.log(aPerson.name);
+for (const course of aPerson.courses) {
+  console.log(course.name, course.isAdvanced);
+}
+console.log(numAdvancedCourses);
