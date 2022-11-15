c5ec031383120cfa80c44bfb49ee4e06f2142106
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-15 23:31:34 +0900

    ✨ 7.3 기본형을 객체로 바꾸기 - Order

---
+++ ch07/7-3-Order.js
@@ -0,0 +1,34 @@
+/**
+ * p252 예시
+ */
+
+class Order {
+  constructor(data) {
+    this.priority = data.priority;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const orders = [
+  new Order({ id: 1, priority: 'low' }),
+  new Order({ id: 2, priority: 'normal' }),
+  new Order({ id: 3, priority: 'high' }),
+  new Order({ id: 4, priority: 'rush' }),
+];
+let highPriorityCount = 0;
+
+/**
+ * 예시 코드 사용
+ */
+
+highPriorityCount = orders.filter(
+  (o) => 'high' === o.priority || 'rush' === o.priority
+).length;
+
+for (const order of orders) {
+  console.log(order.priority);
+}
+console.log(highPriorityCount);
