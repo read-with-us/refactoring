71381c1b42f9f84e8f5e51bc9f8da3fc2a548c2d
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-16 20:50:54 +0900

    ♻️ 7.4 임시 변수를 질의 함수로 바꾸기 - Order (3)

--- ch07/7-4-Order.js
+++ ch07/7-4-Order.js
@@ -11,14 +11,13 @@ class Order {
   }
 
   get price() {
-    const basePrice = this.basePrice;
     var discountFactor = 0.98;
 
-    if (basePrice > 1000) {
+    if (this.basePrice > 1000) {
       discountFactor -= 0.03;
     }
 
-    return basePrice * discountFactor;
+    return this.basePrice * discountFactor;
   }
 
   get basePrice() {
