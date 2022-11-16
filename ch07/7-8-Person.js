d6eb19f93b38be5d338f908674150629150438b9
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-16 23:38:05 +0900

    ✨ 7.8 중개자 제거하기 - Person

---
+++ ch07/7-8-Person.js
@@ -0,0 +1,55 @@
+/**
+ * p272 예시
+ */
+
+class Person {
+  #name;
+  #department;
+  constructor(name, department) {
+    this.#name = name;
+    this.#department = department;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get manager() {
+    return this.#department.manager;
+  }
+  get chargeCode() {
+    return this.#department.chargeCode;
+  }
+}
+
+class Department {
+  #chargeCode;
+  #manager;
+  constructor(chargeCode, manager) {
+    this.#chargeCode = chargeCode;
+    this.#manager = manager;
+  }
+
+  get chargeCode() {
+    return this.#chargeCode;
+  }
+  set chargeCode(arg) {
+    this.#chargeCode = arg;
+  }
+  get manager() {
+    return this.#manager;
+  }
+  set manager(arg) {
+    this.#manager = arg;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const name = '마틴 파울러';
+const chargeCode = 'AA';
+const manager = '레베카 파슨스';
+const person = new Person(name, new Department(chargeCode, manager));
+
+console.log(person.name, person.chargeCode, person.manager);
