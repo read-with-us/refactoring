86b647dd1d98d32ed124c200a7efd7af0979a0c0
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-16 21:02:34 +0900

    ✨ 7.5 클래스 추출하기 - Person

---
+++ ch07/7-5-Person.js
@@ -0,0 +1,47 @@
+/**
+ * p261 예시
+ */
+
+class Person {
+  #name;
+  #officeAreaCode;
+  #officeNumber;
+  constructor(name, officeAreaCode, officeNumber) {
+    this.#name = name;
+    this.#officeAreaCode = officeAreaCode;
+    this.#officeNumber = officeNumber;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  set name(arg) {
+    this.#name = arg;
+  }
+  get officeAreaCode() {
+    return this.#officeAreaCode;
+  }
+  set officeAreaCode(arg) {
+    this.#officeAreaCode = arg;
+  }
+  get officeNumber() {
+    return this.#officeNumber;
+  }
+  set officeNumber(arg) {
+    this.#officeNumber = arg;
+  }
+  get telephoneNumber() {
+    return `${this.officeAreaCode} ${this.officeNumber}`;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const name = '마틴 파울러';
+const officeAreaCode = '02';
+const officeNumber = '1231234';
+const person = new Person(name, officeAreaCode, officeNumber);
+
+console.log(person.name, person.telephoneNumber);
