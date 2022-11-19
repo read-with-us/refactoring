5f8baac6bff00ae45e225b65d0a2da72f2322a71
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-19 18:56:02 +0900

    ♻️  8.7 반복문 쪼개기 - youngest (3)

--- ch08/8-7-youngest.js
+++ ch08/8-7-youngest.js
@@ -3,16 +3,23 @@
  */
 
 function main() {
+  return `최연소: ${youngestAge()}, 총급여: ${totalSalary()}`;
+}
+
+function totalSalary() {
   let totalSalary = 0;
   for (const p of people) {
     totalSalary += p.salary;
   }
+  return totalSalary;
+}
+
+function youngestAge() {
   let youngest = people[0] ? people[0].age : Infinity;
   for (const p of people) {
     if (p.age < youngest) youngest = p.age;
   }
-
-  return `최연소: ${youngest}, 총급여: ${totalSalary}`;
+  return youngest;
 }
 
 /**
