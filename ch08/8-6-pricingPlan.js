35b6c9c635781f86a2a7ece198b2da597a19e110
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-19 18:43:05 +0900

    ♻️  8.6 문장 슬라이드하기 - pricingPlan (3)

--- ch08/8-6-pricingPlan.js
+++ ch08/8-6-pricingPlan.js
@@ -8,11 +8,11 @@ let charge;
 const chargePerUnit = pricingPlan.unit;
 const order = retrieveOrder();
 const units = order.units;
-charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
 let discount;
 discount = discountableUnits * pricingPlan.discountFactor;
 if (order.isRepeat) discount += 20;
+charge = baseCharge + units * chargePerUnit;
 charge = charge - discount;
 chargeOrder(charge);
 
