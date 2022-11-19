7b4b1da36e6c0e45825f4335ead1cebe99c366a0
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-19 18:53:13 +0900

    ♻️  8.7 반복문 쪼개기 - youngest (2)

--- ch08/8-7-youngest.js
+++ ch08/8-7-youngest.js
@@ -3,15 +3,13 @@
  */
 
 function main() {
-  let youngest = people[0] ? people[0].age : Infinity;
   let totalSalary = 0;
   for (const p of people) {
-    // 부수효과가 있는 코드는 한쪽만 남기고 제거
     totalSalary += p.salary;
   }
+  let youngest = people[0] ? people[0].age : Infinity;
   for (const p of people) {
     if (p.age < youngest) youngest = p.age;
-    // 부수효과가 있는 코드는 한쪽만 남기고 제거
   }
 
   return `최연소: ${youngest}, 총급여: ${totalSalary}`;
