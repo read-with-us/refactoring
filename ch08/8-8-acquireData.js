d7be624c5184cec7b24dd19e7b0bac1e2c2e1d86
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-19 19:22:51 +0900

    ♻️  8.8 반복문을 파이프라인으로 바꾸기 - acquireData (3)

--- ch08/8-8-acquireData.js
+++ ch08/8-8-acquireData.js
@@ -7,9 +7,8 @@ import { readFileSync } from 'fs';
 function acquireData(input) {
   const lines = input.split('\n');
   const result = [];
-  const loopItems = lines.slice(1);
+  const loopItems = lines.slice(1).filter((line) => line.trim() !== '');
   for (const line of loopItems) {
-    if (line.trim() === '') continue;
     const record = line.split(',');
     if (record[1].trim() === 'India') {
       result.push({ city: record[0].trim(), phone: record[2].trim() });
