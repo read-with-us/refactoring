5ab53225ae337d72eba7b58e8c98aecc32dfbcb8
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-19 17:47:41 +0900

    ♻️  8.3 문장을 함수로 옮기기 - renderPerson (1)

--- ch08/8-3-renderPerson.js
+++ ch08/8-3-renderPerson.js
@@ -7,18 +7,12 @@ function renderPerson(outStream, person) {
 
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(`<p>제목: ${person.photo.title}</p>`); // <- 제목 출력
-  result.push(emitPhotoData(person.photo));
+  result.push(zznew(person.photo));
   return result.join('\n');
 }
 
 function photoDiv(p) {
-  return [
-    '<div>',
-    `<p>제목: ${p.title}</p>`, // <- 제목 출력
-    emitPhotoData(p),
-    '</div>',
-  ].join('\n');
+  return ['<div>', zznew(p), '</div>'].join('\n');
 }
 
 function emitPhotoData(aPhoto) {
@@ -26,6 +20,10 @@ function emitPhotoData(aPhoto) {
   result.push(`<p>위치: ${aPhoto.location}</p>`);
   result.push(`<p>날짜: ${aPhoto.date.toString()}</p>`);
   return result.join('\n');
+}
+
+function zznew(p) {
+  return [`<p>제목: ${p.title}</p>`, emitPhotoData(p)].join('\n');
 }
 
 /**
