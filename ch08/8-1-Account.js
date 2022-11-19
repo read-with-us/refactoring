0ede925d9d7ebf2eb920615ad8d5e567d2446b7a
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-19 16:47:27 +0900

    ♻️  8.1 함수 옮기기 - Account (1)

--- ch08/8-1-Account.js
+++ ch08/8-1-Account.js
@@ -17,14 +17,12 @@ class Account {
     return this.#daysOverdrawn;
   }
 
-  // 은행 이자 계산
   get bankCharge() {
     let result = 4.5;
     if (this.#daysOverdrawn > 0) result += this.overdraftCharge;
     return result;
   }
 
-  // 초과 인출 이자 계산
   get overdraftCharge() {
     if (this.type.isPremium) {
       const baseCharge = 10;
@@ -46,6 +44,19 @@ class AccountType {
   }
   get isPremium() {
     return this.#type === 'Premium';
+  }
+
+  overdraftCharge(daysOverdrawn) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (daysOverdrawn <= 7) {
+        return baseCharge;
+      } else {
+        return baseCharge + (daysOverdrawn - 7) * 0.85;
+      }
+    } else {
+      return daysOverdrawn * 1.75;
+    }
   }
 }
 
