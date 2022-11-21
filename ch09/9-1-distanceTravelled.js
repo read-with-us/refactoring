099926dc36963a12e891bb86d0a0f260e296941a
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-21 22:28:39 +0900

    ♻️  9.1 변수 쪼개기 - distanceTravelled (1)

--- ch09/9-1-distanceTravelled.js
+++ ch09/9-1-distanceTravelled.js
@@ -4,14 +4,13 @@
 
 function distanceTravelled(scenario, time) {
   let result;
-  let acc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
+  const primaryAcceleration = scenario.primaryForce / scenario.mass;
   let primaryTime = Math.min(time, scenario.delay);
-  result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+  result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
   let secondaryTime = time - scenario.delay;
-  // 두 번째 힘을 반영해 다시 계산
   if (secondaryTime > 0) {
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    let primaryVelocity = primaryAcceleration * scenario.delay;
+    let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
     result +=
       primaryVelocity * secondaryTime +
       0.5 * acc * secondaryTime * secondaryTime;
