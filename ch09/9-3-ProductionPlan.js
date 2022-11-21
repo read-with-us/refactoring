1422c9b922e462ae7d553254c453a670a3e72897
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-21 23:40:08 +0900

    ♻️  9.3 파생 변수를 질의 함수로 바꾸기 - ProductionPlan (3)

--- ch09/9-3-ProductionPlan.js
+++ ch09/9-3-ProductionPlan.js
@@ -14,9 +14,6 @@ class ProductionPlan {
   }
 
   get production() {
-    return this.#production;
-  }
-  get calculatedProduction() {
     return this.#adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
   applyAdjustment(anAdjustment) {
