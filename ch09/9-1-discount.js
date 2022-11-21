b182f01fd21b47e03f42a4348092e19c3c95f80f
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-21 22:35:54 +0900

    ♻️  9.1 변수 쪼개기 - discount (2)

--- ch09/9-1-discount.js
+++ ch09/9-1-discount.js
@@ -2,11 +2,11 @@
  * p333 예시
  */
 
-function discount(originalInputValue, quantity) {
-  let inputValue = originalInputValue;
-  if (inputValue > 50) inputValue = inputValue - 2;
-  if (quantity > 100) inputValue = inputValue - 1;
-  return inputValue;
+function discount(inputValue, quantity) {
+  let result = inputValue;
+  if (result > 50) result = result - 2;
+  if (quantity > 100) result = result - 1;
+  return result;
 }
 
 /**
