6dd225e24a266bcbd2c1e00ac394060855335fcc
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-21 22:49:21 +0900

    ♻️  9.2 필드 이름 바꾸기 - organization (1)

--- ch09/9-2-organization.js
+++ ch09/9-2-organization.js
@@ -2,7 +2,32 @@
  * p335 예시
  */
 
-const organization = { name: '애크미 구스베리', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  constructor(data) {
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  set name(aString) {
+    this.#name = aString;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(aCountryCode) {
+    this.#country = aCountryCode;
+  }
+}
+
+const organization = new Organization({
+  name: '애크미 구스베리',
+  country: 'GB',
+});
 
 /**
  * 예시 실행을 위한 임의의 코드
