3af573be63852e07b89205449190cd78db7644f8
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-25 22:12:37 +0900

    ♻️  10.1 조건문 분해하기 - charge (2)

--- ch10/10-1-charge.js
+++ ch10/10-1-charge.js
@@ -5,7 +5,7 @@
 function charge(quantity, plan) {
   let charge;
   if (summer()) {
-    charge = quantity * plan.summerRate;
+    charge = summerCharge();
   } else {
     charge = quantity * plan.regularRate + plan.regularServiceCharge;
   }
@@ -13,6 +13,10 @@ function charge(quantity, plan) {
 
   function summer() {
     return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
   }
 }
 
