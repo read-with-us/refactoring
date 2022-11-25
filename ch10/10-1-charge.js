4947f122c2ea95100c019fa893ba3795bc843028
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-25 22:09:18 +0900

    ✨ 10.1 조건문 분해하기 - charge

---
+++ ch10/10-1-charge.js
@@ -0,0 +1,36 @@
+/**
+ * p355 예시
+ */
+
+function charge(quantity, plan) {
+  let charge;
+  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+    charge = quantity * plan.summerRate;
+  } else {
+    charge = quantity * plan.regularRate + plan.regularServiceCharge;
+  }
+  return charge;
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const aDate = {
+  isBefore: (date) => {
+    return date > '2022-07-25';
+  },
+  isAfter: (date) => {
+    return date < '2022-07-25';
+  },
+};
+const quantity = 30;
+const plan = {
+  summerStart: '2022-06-01',
+  summerEnd: '2022-08-31',
+  summerRate: 0.8,
+  regularRate: 1,
+  regularServiceCharge: 100,
+};
+
+console.log(charge(quantity, plan));
