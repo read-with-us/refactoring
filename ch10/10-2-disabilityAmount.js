777f1e9f4f13ea8a5e1b505ffd4bccd77ec6453a
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-25 22:34:30 +0900

    ♻️  10.2 조건식 통합하기 - disabilityAmount (3)

--- ch10/10-2-disabilityAmount.js
+++ ch10/10-2-disabilityAmount.js
@@ -3,13 +3,17 @@
  */
 
 function disabilityAmount(anEmployee) {
-  if (
-    anEmployee.seniority < 2 ||
-    anEmployee.monthsDisabled > 12 ||
-    anEmployee.isPartTime
-  )
-    return 0;
+  if (isNotEligibleForDiability()) return 0;
   // 장애 수당 계산
+
+  function isNotEligibleForDiability() {
+    // 장애 수당 적용 여부 확인
+    return (
+      anEmployee.seniority < 2 ||
+      anEmployee.monthsDisabled > 12 ||
+      anEmployee.isPartTime
+    );
+  }
 }
 
 /**
