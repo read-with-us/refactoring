aa4ea62eae6996d0668511ea90d6fa21ae13fd63
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-25 22:47:29 +0900

    ♻️  10.3 중첩 조건문을 보호 구문으로 바꾸기 - payAmount (1)

--- ch10/10-3-payAmount.js
+++ ch10/10-3-payAmount.js
@@ -5,16 +5,13 @@
 function payAmount(employee) {
   let result;
   if (employee.isSeparated) {
-    // 퇴사한 직원인가?
-    result = { amount: 0, reasonCode: 'SEP' };
+    return { amount: 0, reasonCode: 'SEP' };
+  }
+  if (employee.isRetired) {
+    result = { amount: 0, reasonCode: 'RET' };
   } else {
-    if (employee.isRetired) {
-      // 은퇴한 직원인가?
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // 급여 계산 로직
-      result = someFinalComputation();
-    }
+    // 급여 계산 로직
+    result = someFinalComputation();
   }
   return result;
 }
