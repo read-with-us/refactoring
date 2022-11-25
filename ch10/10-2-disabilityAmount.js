847923e3f0c0b547c9b05dadc58dacead718a0ce
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-25 22:25:59 +0900

    ✨ 10.2 조건식 통합하기 - disabilityAmount

---
+++ ch10/10-2-disabilityAmount.js
@@ -0,0 +1,21 @@
+/**
+ * p358 예시
+ */
+
+function disabilityAmount(anEmployee) {
+  if (anEmployee.seniority < 2) return 0;
+  if (anEmployee.monthsDisabled > 12) return 0;
+  if (anEmployee.isPartTime) return 0;
+  // 장애 수당 계산
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+const employee = {
+  seniority: 1,
+  monthsDisabled: 11,
+  isPartTime: true,
+};
+
+console.log(disabilityAmount(employee));
