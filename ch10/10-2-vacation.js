8876aef9a2f9094347a6bcff316863e75d8e5e94
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-25 22:39:18 +0900

    ✨ 10.2 조건식 통합하기 - vacation

---
+++ ch10/10-2-vacation.js
@@ -0,0 +1,23 @@
+/**
+ * p359 예시
+ */
+
+function vacation(anEmployee) {
+  if (anEmployee.onVacation) {
+    if (anEmployee.seniority > 10) {
+      return 1;
+    }
+    return 0.5;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const anEmployee = {
+  onVacation: true,
+  seniority: 12,
+};
+
+console.log(vacation(anEmployee));
