d6597d4c575e6f164d28f4ea2de41a4f25302373
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-26 13:32:02 +0900

    ♻️  10.3 중첩 조건문을 보호 구문으로 바꾸기 - adjustedCapital (5)

--- ch10/10-3-adjustedCapital.js
+++ ch10/10-3-adjustedCapital.js
@@ -3,17 +3,16 @@
  */
 
 function adjustedCapital(anInstrument) {
-  let result = 0;
   if (
     anInstrument.capital <= 0 ||
     anInstrument.interestRate <= 0 ||
     anInstrument.duration <= 0
   )
-    return result;
-  result =
+    return 0;
+  return (
     (anInstrument.income / anInstrument.duration) *
-    anInstrument.adjustmentFactor;
-  return result;
+    anInstrument.adjustmentFactor
+  );
 }
 
 /**
