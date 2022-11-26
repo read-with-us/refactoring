802078995d80a3371c59d289bd8a92938084e313
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-26 23:47:41 +0900

    ✨ 10.5 특이 케이스 추가하기 - acquireSiteData

---
+++ ch10/10-5-acquireSiteData.js
@@ -0,0 +1,47 @@
+/**
+ * p398 예시
+ */
+
+const siteData = {
+  name: '물류창고 15',
+  location: 'Malden MA',
+  // 더 많은 현장(site) 정보
+  customer: '미확인 고객',
+};
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+function acquireSiteData() {
+  return siteData;
+}
+
+const registry = {
+  billingPlan: {
+    basic: 'basic',
+  },
+};
+
+/**
+ * 예시 코드 사용
+ */
+
+const site = acquireSiteData();
+const aCustomer = site.customer;
+// ... 수많은 코드 ...
+let customerName;
+if (aCustomer === '미확인 고객') customerName = '거주자';
+else customerName = aCustomer.name;
+
+const plan =
+  aCustomer === '미확인 고객'
+    ? registry.billingPlan.basic
+    : aCustomer.billingPlan;
+
+const weeksDelinquent =
+  aCustomer === '미확인 고객'
+    ? 0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+
+console.log(customerName, plan, weeksDelinquent);
