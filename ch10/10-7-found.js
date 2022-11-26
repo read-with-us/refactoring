46957ea49ce55ed7c50cb7800ac859d7a0933541
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-27 00:23:24 +0900

    ♻️  10.7 제어 플래그를 탈출문으로 바꾸기 - found (4)

--- ch10/10-7-found.js
+++ ch10/10-7-found.js
@@ -9,17 +9,14 @@ function found() {
 }
 
 function checkForMiscreants() {
-  let found = false;
   for (const p of people) {
-    if (!found) {
-      if (p === '조커') {
-        sendAlert();
-        return;
-      }
-      if (p === '사루만') {
-        sendAlert();
-        return;
-      }
+    if (p === '조커') {
+      sendAlert();
+      return;
+    }
+    if (p === '사루만') {
+      sendAlert();
+      return;
     }
   }
 }
