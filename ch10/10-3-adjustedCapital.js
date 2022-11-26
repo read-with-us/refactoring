2afe66029bc7919c1362e4e5083a8288eb528829
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-26 13:25:25 +0900

    ✨ 10.3 중첩 조건문을 보호 구문으로 바꾸기 - adjustedCapital

---
+++ ch10/10-3-adjustedCapital.js
@@ -0,0 +1,29 @@
+/**
+ * p363 예시
+ */
+
+function adjustedCapital(anInstrument) {
+  let result = 0;
+  if (anInstrument.capital > 0) {
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+      result =
+        (anInstrument.income / anInstrument.duration) *
+        anInstrument.adjustmentFactor;
+    }
+  }
+  return result;
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const instrument = {
+  capital: 3,
+  interestRate: 0.2,
+  duration: 10,
+  income: 10000,
+  adjustmentFactor: 0.3,
+};
+
+console.log(adjustedCapital(instrument));
