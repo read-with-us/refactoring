c048d32bd7d88a2437860c88ac7587f28ed59aff
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-26 14:03:43 +0900

    ♻️  10.4 조건부 로직을 다형성으로 바꾸기 - plumages (7)

--- ch10/10-4-plumages.js
+++ ch10/10-4-plumages.js
@@ -17,19 +17,17 @@ class Bird {
 }
 
 function plumages(birds) {
-  return new Map(birds.map((b) => [b.name, plumage(b)]));
+  return new Map(
+    birds.map((b) => createBird(b)).map((bird) => [bird.name, bird.plumage])
+  );
 }
 
 function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
-}
-
-function plumage(bird) {
-  return createBird(bird).plumage;
-}
-
-function airSpeedVelocity(bird) {
-  return createBird(bird).airSpeedVelocity;
+  return new Map(
+    birds
+      .map((b) => createBird(b))
+      .map((bird) => [bird.name, bird.airSpeedVelocity])
+  );
 }
 
 function createBird(bird) {
