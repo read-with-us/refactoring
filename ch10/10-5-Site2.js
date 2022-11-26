aeb59534667919722f82169df47e24c9d34e9518
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-26 23:31:15 +0900

    ✨ 10.5 특이 케이스 추가하기 - Site2

---
+++ ch10/10-5-Site2.js
@@ -0,0 +1,73 @@
+/**
+ * p395 예시
+ */
+
+class Site {
+  #customer;
+  constructor(customer) {
+    this.#customer = customer;
+  }
+
+  get customer() {
+    return this.#customer;
+  }
+}
+
+class Customer {
+  #name;
+  #billingPlan;
+  #paymentHistory;
+  constructor(name, billingPlan, paymentHistory) {
+    this.#name = name;
+    this.#billingPlan = billingPlan;
+    this.#paymentHistory = paymentHistory;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get billingPlan() {
+    return this.#billingPlan;
+  }
+  set billingPlan(arg) {
+    this.#billingPlan = arg;
+  }
+  get paymentHistory() {
+    return this.#paymentHistory;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const customer = '미확인 고객';
+const site = new Site(customer);
+
+const registry = {
+  billingPlan: {
+    basic: 'basic',
+  },
+};
+
+/**
+ * 예시 코드 사용
+ */
+
+const aCustomer = site.customer;
+// ... 수많은 코드 ...
+let customerName;
+if (aCustomer === '미확인 고객') customerName = '거주자';
+else customerName = aCustomer.name;
+
+const plan =
+  aCustomer === '미확인 고객'
+    ? registry.billingPlan.basic
+    : aCustomer.billingPlan;
+
+const weeksDelinquent =
+  aCustomer === '미확인 고객'
+    ? 0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+
+console.log(customerName, plan, weeksDelinquent);
