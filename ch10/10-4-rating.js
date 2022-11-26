1cfbefa98501f8509a193959f324321caa8c93fc
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-26 22:00:43 +0900

    ♻️  10.4 조건부 로직을 다형성으로 바꾸기 - rating (9)

--- ch10/10-4-rating.js
+++ ch10/10-4-rating.js
@@ -47,13 +47,13 @@ class Rating {
     let result = 2;
     if (this.#voyage.zone === '중국') result += 1;
     if (this.#voyage.zone === '동인동') result += 1;
+    result += this.historyLengthFactor;
     result += this.voyageAndHistoryLengthFactor;
     return result;
   }
 
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    result += this.historyLengthFactor;
     if (this.#voyage.length > 14) result -= 1;
     return result;
   }
@@ -80,7 +80,6 @@ class ExperiencedChinaRating extends Rating {
   get voyageAndHistoryLengthFactor() {
     let result = 0;
     result += 3;
-    result += this.historyLengthFactor;
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
     return result;
