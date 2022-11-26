ac3bca546343e49f378b771453356308b1847ffc
yisu-kim <yisu.kim.dev@outlook.com>
2022-11-27 00:15:05 +0900

    ♻️  10.6 어서션 추가하기 - Customer (3)

--- ch10/10-6-Customer.js
+++ ch10/10-6-Customer.js
@@ -12,11 +12,14 @@ class Customer {
   get discountRate() {
     return this.#discountRate;
   }
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this.#discountRate = aNumber;
+  }
 
   applyDiscount(aNumber) {
     if (!this.#discountRate) return aNumber;
     else {
-      assert(this.#discountRate >= 0);
       return aNumber - this.#discountRate * aNumber;
     }
   }
