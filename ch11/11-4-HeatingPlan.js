77be15153146b52a3d08b7b2fc7902f7ae15cd89
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 00:26:35 +0900

    ✨ 11.4 객체 통째로 넘기기 - HeatingPlan

---
+++ ch11/11-4-HeatingPlan.js
@@ -0,0 +1,43 @@
+/**
+ * p428 예시
+ */
+
+class HeatingPlan {
+  #temperatureRange;
+  constructor(low, high) {
+    this.#temperatureRange = { low, high };
+  }
+
+  withinRange(bottom, top) {
+    return (
+      bottom >= this.#temperatureRange.low && top <= this.#temperatureRange.high
+    );
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const aRoom = {
+  daysTempRange: {
+    low: -7,
+    high: 13,
+  },
+};
+
+const aPlan = new HeatingPlan(0, 20);
+
+const alerts = [];
+
+/**
+ * 예시 호출
+ */
+
+const low = aRoom.daysTempRange.low;
+const high = aRoom.daysTempRange.high;
+if (!aPlan.withinRange(low, high)) {
+  alerts.push('방 온도가 지정 범위를 벗어났습니다.');
+}
+
+console.log(alerts);
