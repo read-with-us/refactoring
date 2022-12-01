ad88377b99bfd0f66a35bcd556c04c7014f11a75
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-01 20:33:22 +0900

    ✨ 11.1 질의 함수와 변경 함수 분리하기 - alertForMiscreant

---
+++ ch11/11-1-alertForMiscreant.js
@@ -0,0 +1,30 @@
+/**
+ * p414 예시
+ */
+
+function alertForMiscreant(people) {
+  for (const p of people) {
+    if (p === '조커') {
+      setOffAlarms();
+      return '조커';
+    }
+    if (p === '사루만') {
+      setOffAlarms();
+      return '사루만';
+    }
+  }
+  return '';
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+function setOffAlarms() {
+  console.log('set off alarms');
+}
+
+const people = ['배트맨', '조커'];
+const found = alertForMiscreant(people);
+
+console.log(found);
