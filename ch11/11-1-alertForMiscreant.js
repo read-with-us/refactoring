b99837dce9cf23e90c19ed92fdc33ee6c4fc9c5c
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-01 20:45:56 +0900

    ♻️  11.1 질의 함수와 변경 함수 분리하기 - alertForMiscreant (5)

--- ch11/11-1-alertForMiscreant.js
+++ ch11/11-1-alertForMiscreant.js
@@ -3,17 +3,7 @@
  */
 
 function alertForMiscreant(people) {
-  for (const p of people) {
-    if (p === '조커') {
-      setOffAlarms();
-      return;
-    }
-    if (p === '사루만') {
-      setOffAlarms();
-      return;
-    }
-  }
-  return;
+  if (findMiscreant(people) !== '') setOffAlarms();
 }
 
 function findMiscreant(people) {
