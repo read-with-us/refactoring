9a8dc0731e64410fa8bb086bed47f41332d97f95
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 10:50:45 +0900

    ✨ 11.6 질의 함수를 매개변수로 바꾸기 - HeatingPlan

---
+++ ch11/11-6-HeatingPlan.js
@@ -0,0 +1,47 @@
+/**
+ * p437 예시
+ */
+
+class HeatingPlan {
+  #min;
+  #max;
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+  get targetTemperature() {
+    if (thermostat.selectedTemperature > this.#max) return this.#max;
+    else if (thermostat.selectedTemperature < this.#min) return this.#min;
+    else return thermostat.selectedTemperature;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const thermostat = {
+  selectedTemperature: 24,
+  currentTemperature: 17,
+};
+const thePlan = new HeatingPlan(0, 25);
+
+function setToHeat() {
+  console.log('set to heat');
+}
+
+function setToCool() {
+  console.log('set to cool');
+}
+
+function setOff() {
+  console.log('set off');
+}
+
+/**
+ * 예시 호출
+ */
+
+if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+else setOff();
