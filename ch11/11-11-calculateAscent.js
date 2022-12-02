14c5a022ace5587f4689b2dbf5e42b7afce6bc1c
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 20:21:57 +0900

    ♻️  11.11 수정된 값 반환하기 - calculateAscent (3)

--- ch11/11-11-calculateAscent.js
+++ ch11/11-11-calculateAscent.js
@@ -8,10 +8,9 @@ const points = [{ elevation: 5000 }, { elevation: 8000 }];
  * p462 예시
  */
 
-let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
-totalAscent = calculateAscent();
+const totalAscent = calculateAscent();
 calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
