66721ae5a78438091ee98556795976bc1cb6b2be
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 10:27:54 +0900

    ♻️  11.5 매개변수를 질의 함수로 바꾸기 - Order (1)

--- ch11/11-5-Order.js
+++ ch11/11-5-Order.js
@@ -12,10 +12,11 @@ class Order {
 
   get finalPrice() {
     const basePrice = this.#quantity * this.#itemPrice;
-    let discountLevel;
-    if (this.#quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+    return this.discountedPrice(basePrice, this.discountLevel);
+  }
+
+  get discountLevel() {
+    return this.#quantity > 100 ? 2 : 1;
   }
 
   discountedPrice(basePrice, discountLevel) {
