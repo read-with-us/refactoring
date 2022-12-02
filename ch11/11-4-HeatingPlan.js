afc710f5ad74e9f447959f6959a13fce26c6606b
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 09:07:53 +0900

    ♻️  11.4 객체 통째로 넘기기 - HeatingPlan (2)

--- ch11/11-4-HeatingPlan.js
+++ ch11/11-4-HeatingPlan.js
@@ -39,7 +39,7 @@ const alerts = [];
 
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
-if (!aPlan.withinRange(low, high)) {
+if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange)) {
   alerts.push('방 온도가 지정 범위를 벗어났습니다.');
 }
 
