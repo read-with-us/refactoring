9d63b94dacd1102251bcc4df62a8a05ec9c7a4f1
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 20:58:59 +0900

    ♻️  11.13 예외를 사전확인으로 바꾸기 - ResourcePool (4)

--- ch11/11-13-ResourcePool.java
+++ ch11/11-13-ResourcePool.java
@@ -7,11 +7,10 @@ private class ResourcePool {
     Resource result;
     if (available.isEmpty()) {
       result = Resource.create();
-      allocated.add(result);
     } else {
       result = available.pop();
-      allocated.add(result);
     }
+    allocated.add(result);
     return result;
   } 
 }
