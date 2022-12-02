e14eb729cb8300ff7929f55ef4ed16aa2c33a4d2
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-02 20:42:48 +0900

    ♻️  11.12 오류 코드를 예외로 바꾸기 - localShippingRules (2)

--- ch11/11-12-localShippingRules.js
+++ ch11/11-12-localShippingRules.js
@@ -43,7 +43,11 @@ const errorList = [];
  */
 
 let status;
-status = calculateShippingCosts(orderData);
+try {
+  status = calculateShippingCosts(orderData);
+} catch (e) {
+  throw e;
+}
 if (status < 0) errorList.push({ order: orderData, errorCode: status });
 
 console.log(errorList);
