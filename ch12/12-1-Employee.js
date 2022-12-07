95fb32f345ed814173a758619f552a09224c681a
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-07 23:32:54 +0900

    ♻️  12.1 메서드 올리기 - Employee (3)

--- ch12/12-1-Employee.js
+++ ch12/12-1-Employee.js
@@ -6,6 +6,9 @@ class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
+  get monthlyCost() {
+    throw new SubclassResponsibilityError();
+  }
 }
 
 class Employee extends Party {
