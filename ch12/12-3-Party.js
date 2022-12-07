5231b06384cf85d3f5393c943b31da6394c1ccbd
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-08 00:05:08 +0900

    ✨ 12.3 생성자 본문 올리기 - Party

---
+++ ch12/12-3-Party.js
@@ -0,0 +1,51 @@
+/**
+ * p482 예시
+ */
+
+class Party {}
+
+class Employee extends Party {
+  #id;
+  #name;
+  #monthlyCost;
+  constructor(name, id, monthlyCost) {
+    super();
+    this.#id = id;
+    this.#name = name;
+    this.#monthlyCost = monthlyCost;
+  }
+
+  get name() {
+    return this.#name;
+  }
+}
+
+class Department extends Party {
+  #name;
+  #staff;
+  constructor(name, staff) {
+    super();
+    this.#name = name;
+    this.#staff = staff;
+  }
+
+  get name() {
+    return this.#name;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const name = "마틴 파울러";
+const id = 111;
+const monthlyCost = 10000;
+const employee = new Employee(name, id, monthlyCost);
+
+const departmentName = "리팩터링";
+const staff = "마틴 파울러";
+const department = new Department(departmentName, staff);
+
+console.log(employee.name);
+console.log(department.name);
