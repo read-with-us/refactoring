e111297e663ae004a9e2980656bafbcddbcfb93d
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-08 00:19:27 +0900

    ✨ 12.6 타입 코드를 서브클래스로 바꾸기 - Employee

---
+++ ch12/12-6-Employee.js
@@ -0,0 +1,33 @@
+/**
+ * p489 예시
+ */
+
+class Employee {
+  #name;
+  #type;
+  constructor(name, type) {
+    this.validateType(type);
+    this.#name = name;
+    this.#type = type;
+  }
+
+  validateType(arg) {
+    if (!["engineer", "manager", "salesperson"].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+
+  toString() {
+    return `${this.#name} (${this.#type})`;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const name = "마틴 파울러";
+const type = "engineer";
+const employee = new Employee(name, type);
+
+console.log(employee.toString());
