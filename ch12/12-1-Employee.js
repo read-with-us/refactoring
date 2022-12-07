1f2ebcb37d8c3802fb0ca86c73ac0219859d7c05
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-07 22:55:59 +0900

    ✨ 12.1 메서드 올리기 - Employee

---
+++ ch12/12-1-Employee.js
@@ -0,0 +1,35 @@
+/**
+ * p477 예시
+ */
+
+class Party {}
+
+class Employee extends Party {
+  get monthlyCost() {
+    return 100;
+  }
+
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  get monthlyCost() {
+    return 200;
+  }
+
+  get totalAnnualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const employee = new Employee();
+const department = new Department();
+
+console.log(employee.annualCost);
+console.log(department.totalAnnualCost);
