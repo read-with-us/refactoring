951c29a17a0ecff581d13f4d3aee2756e8ac5089
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-08 22:08:55 +0900

    ♻️  12.7 서브클래스 제거하기 - Person (1)

--- ch12/12-7-Person.js
+++ ch12/12-7-Person.js
@@ -43,21 +43,25 @@ const people = loadFromInput(data);
  * 예시 호출
  */
 
+function createPerson(aRecord) {
+  let p;
+  switch (aRecord.gender) {
+    case "M":
+      p = new Male(aRecord.name);
+      break;
+    case "F":
+      p = new Female(aRecord.name);
+      break;
+    default:
+      p = new Person(aRecord.name);
+  }
+  return p;
+}
+
 function loadFromInput(data) {
   const result = [];
   data.forEach((aRecord) => {
-    let p;
-    switch (aRecord.gender) {
-      case "M":
-        p = new Male(aRecord.name);
-        break;
-      case "F":
-        p = new Female(aRecord.name);
-        break;
-      default:
-        p = new Person(aRecord.name);
-    }
-    result.push(p);
+    result.push(createPerson(aRecord));
   });
   return result;
 }
