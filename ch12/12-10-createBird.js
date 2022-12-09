0e1e7280cff3c168d7309733617fb5dc16e73c94
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-09 19:51:42 +0900

    ✨ 12.10 서브클래스를 위임으로 바꾸기 - createBird

---
+++ ch12/12-10-createBird.js
@@ -0,0 +1,99 @@
+/**
+ * p518 예시
+ */
+
+function createBird(data) {
+  switch (data.type) {
+    case "유럽 제비":
+      return new EuropeanSwallow(data);
+    case "아프리카 제비":
+      return new AfricanSwallow(data);
+    case "노르웨이 파랑 앵무":
+      return new NorwegianBlueParrot(data);
+    default:
+      return new Bird(data);
+  }
+}
+
+class Bird {
+  #name;
+  constructor(data) {
+    this.#name = data.name;
+    this._plumage = data.plumage;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get plumage() {
+    return this._plumage || "보통이다";
+  }
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  #numberOfCoconuts;
+  constructor(data) {
+    super(data);
+    this.#numberOfCoconuts = data.numberOfCoconuts;
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.#numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  #voltage;
+  #isNailed;
+  constructor(data) {
+    super(data);
+    this.#voltage = data.voltage;
+    this.#isNailed = data.isNailed;
+  }
+
+  get plumage() {
+    if (this.#voltage > 100) return "그을렸다";
+    else return this._plumage || "예쁘다";
+  }
+  get airSpeedVelocity() {
+    return this.#isNailed ? 0 : 10 + this.#voltage / 10;
+  }
+}
+
+/**
+ * 예시 실행을 위한 임의의 코드
+ */
+
+const data = [
+  {
+    name: "새",
+  },
+  {
+    name: "유럽 제비",
+    type: "유럽 제비",
+  },
+  {
+    name: "아프리카 제비",
+    type: "아프리카 제비",
+    numberOfCoconuts: 2,
+  },
+  {
+    name: "노르웨이 파랑 앵무",
+    type: "노르웨이 파랑 앵무",
+    voltage: 0,
+    isNailed: false,
+  },
+];
+
+const birds = data.map((d) => createBird(d));
+
+birds.forEach((b) => console.log(b.name, b.plumage, b.airSpeedVelocity));
