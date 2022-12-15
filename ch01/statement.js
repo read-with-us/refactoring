55a2318d6476e6beb9ef00b06d4d962bcc34392d
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:21:24 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (1)

--- ch01/statement.js
+++ ch01/statement.js
@@ -17,6 +17,26 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+
+    // 청구 내역을 출력한다.
+    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+
+  function amountFor(perf, play) {
     let thisAmount = 0;
 
     switch (play.type) {
@@ -36,23 +56,8 @@ function statement(invoice, plays) {
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
-
-    // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
 }
 
 /**
