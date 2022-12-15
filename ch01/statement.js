a741b5bf72855b094ce23706297ec1a4505c47be
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:55:12 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (24)

--- ch01/statement.js
+++ ch01/statement.js
@@ -14,9 +14,8 @@ function statement(invoice, plays) {
       perf.audience
     }석)\n`;
   }
-  let totalAmount = appleSauce();
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(appleSauce())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
