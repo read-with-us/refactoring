60d72e4e6450be8404632d428507a42b95839fbd
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:53:24 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (20)

--- ch01/statement.js
+++ ch01/statement.js
@@ -14,6 +14,8 @@ function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
+  }
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
