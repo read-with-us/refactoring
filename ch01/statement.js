42232b510ee83a2ffd9600ba92d8790a76ab4055
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:40:08 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (9)

--- ch01/statement.js
+++ ch01/statement.js
@@ -16,12 +16,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -32,6 +27,15 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+    return volumeCredits;
+  }
 
   function playFor(perf) {
     return plays[perf.playID];
