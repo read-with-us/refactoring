acd54241a65cef9c216e3f0b52dca84e9c2f3719
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:42:29 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (10)

--- ch01/statement.js
+++ ch01/statement.js
@@ -29,12 +29,12 @@ function statement(invoice, plays) {
   return result;
 
   function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
     if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+      result += Math.floor(perf.audience / 5);
     }
-    return volumeCredits;
+    return result;
   }
 
   function playFor(perf) {
