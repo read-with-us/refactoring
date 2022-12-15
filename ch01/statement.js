3aaffad811f974428beb9ef0ba3f90ce3224fdc1
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:35:04 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (7)

--- ch01/statement.js
+++ ch01/statement.js
@@ -16,7 +16,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -39,7 +39,7 @@ function statement(invoice, plays) {
     return plays[perf.playID];
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
     switch (playFor(aPerformance).type) {
