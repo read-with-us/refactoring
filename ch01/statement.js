335aa57339163dde8bd3c761e6ae24eb805d9907
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:34:35 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (6)

--- ch01/statement.js
+++ ch01/statement.js
@@ -42,7 +42,7 @@ function statement(invoice, plays) {
   function amountFor(aPerformance, play) {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -57,7 +57,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
     return result;
   }
