1f03201accf35d7c983263f3805fef4c2864795a
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 00:47:05 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (15)

--- ch01/statement.js
+++ ch01/statement.js
@@ -11,14 +11,16 @@ function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
