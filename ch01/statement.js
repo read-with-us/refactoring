c6df64850e1084602975ec1e2529ca5ad580d030
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 22:00:19 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (38)

--- ch01/statement.js
+++ ch01/statement.js
@@ -21,14 +21,32 @@ function renderPlainText(data) {
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "  <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 /**
@@ -36,7 +54,13 @@ function renderPlainText(data) {
  */
 
 console.log("\n================================\n");
+
 invoices.forEach((invoice) => {
   console.log(statement(invoice, plays));
+  console.log("================================\n");
+});
+
+invoices.forEach((invoice) => {
+  console.log(htmlStatement(invoice, plays));
   console.log("================================\n");
 });
