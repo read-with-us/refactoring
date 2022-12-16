134525af86644ec9b34a9ac1b096bcffee0df5f7
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 22:33:31 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (46)

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -76,5 +76,16 @@ class PerformanceCalculator {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
