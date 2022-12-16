1022b7e9ef327c1853239eb939669380478574c5
yisu-kim <yisu.kim.dev@outlook.com>
2022-12-16 21:11:33 +0900

    ♻️  1. 리팩터링: 첫 번째 예시 - statement (28)

--- ch01/statement.js
+++ ch01/statement.js
@@ -7,11 +7,12 @@ import plays from "./plays.json" assert { type: "json" };
 
 function statement(invoice, plays) {
   const statementData = {};
+  statementData.customer = invoice.customer;
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
